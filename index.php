7ff1536b5de794e55bd06ca8540ee15b611fb663
Oneide Luiz Schneider <oneide_ls@yahoo.com.br>
2015-05-02 00:59:49 -0300

    Exemplo Memcached

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	$memcache = new Memcache;
+    $memcache->connect("localhost",11211); 
+                                          
+    echo "Versão Memcached : " . $memcache->getVersion();
+     
+    $testeArray = array('Oneide', 'Luiz', 'Schneider');
+    $temp       = serialize($testArray);
+    $memcache->add("key", $temp, false, 30);
+ 
+    echo "Dados em cache:<br />\n";
+    print_r(unserialize($memcache->get("key")));
